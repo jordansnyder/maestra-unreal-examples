4e0a1128099afc91801cc41de2d26300845f87ac
Jordan Snyder <jordan.snyder@gmail.com>
2026-04-04 10:48:31 -0600

    updated build batch file

--- UE5Aurora/PackageDev.bat
+++ UE5Aurora/PackageDev.bat
@@ -6,21 +6,20 @@ echo  MaestraUnrealClean - Package for Windows
 echo ============================================
 echo.
 
-echo Cleaning previous build...
-if exist "Saved\Cooked" rmdir /s /q "Saved\Cooked"
-if exist "Saved\StagedBuilds" rmdir /s /q "Saved\StagedBuilds"
-if exist "Windows" rmdir /s /q "Windows"
+REM echo Cleaning previous staged build...
+REM if exist "Saved\StagedBuilds" rmdir /s /q "Saved\StagedBuilds"
+REM if exist "Windows" rmdir /s /q "Windows"
 
 echo Packaging...
-call "C:\Program Files\Epic Games\UE_5.7\Engine\Build\BatchFiles\RunUAT.bat" ^
+call "D:\Epic Games\UE_5.7\Engine\Build\BatchFiles\RunUAT.bat" ^
   BuildCookRun ^
-  -project=C:\Users\jorda\Dev\maestra-unreal-examples\UE5Aurora\MaestraUnrealClean.uproject ^
-  -nop4 -utf8output -cook -build -stage -archive -package -pak -iostore -compressed -prereqs ^
+  -project=C:\Users\Jordan\Documents\GitHub\maestra-unreal-examples\UE5Aurora\MaestraUnrealClean.uproject ^
+  -nop4 -utf8output -cook -build -stage -archive -package -pak -iostore -compressed -prereqs -legacyiterative ^
   -platform=Win64 ^
   -clientconfig=Development ^
-  -archivedirectory=C:\Users\jorda\Dev\maestra-unreal-examples\UE5Aurora ^
+  -archivedirectory=C:\Users\Jordan\Documents\GitHub\maestra-unreal-examples\UE5Aurora ^
   -target=MaestraUnrealClean ^
-  -unrealexe="C:\Program Files\Epic Games\UE_5.7\Engine\Binaries\Win64\UnrealEditor-Cmd.exe"
+  -unrealexe="D:\Epic Games\UE_5.7\Engine\Binaries\Win64\UnrealEditor-Cmd.exe"
 
 if %ERRORLEVEL% EQU 0 (
   echo.
